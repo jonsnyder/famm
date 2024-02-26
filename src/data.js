aaddf7cc4f69423d671995cf1cb871ce60806cde
Jon Snyder <josnyder@adobe.com>
2024-02-26 14:54:05 -0700

    Update data layer and don't navigate on add to cart

--- src/data.js
+++ src/data.js
@@ -1,9 +1,13 @@
 module.exports = ({ name, variables }) => {
-  return {
+  const data = {
     ...variables,
     digitalData: {
-      page: name,
-      products: variables.products
+      page: name
     }
+  };
+  if (name === "product") {
+    data.digitalData.product = variables.products[0];
   }
+  data.digitalData.products = variables.products;
+  return data;
 };
